5d720000f6efd6a43beb7770dc4ea3ae5fc48fae
MikeFreno <michaelt.freno@gmail.com>
2023-07-21 15:49:08 -0400

    potential fix for home layout changing on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,76 +131,74 @@ export default function Home() {
               />
             </div>
             <nav className="mx-auto flex flex-row">
-              {windowWidth == 0 || windowWidth > 768 ? (
-                <div
-                  ref={linkRef}
-                  className="flex w-fit flex-row transition-all duration-400 ease-in-out"
+              <div
+                ref={linkRef}
+                className="w-fit flex-row transition-all duration-400 ease-in-out hidden md:flex"
+              >
+                <button
+                  ref={aboutButtonRef}
+                  onClick={aboutToggle}
+                  className="w-20 rounded border border-white py-2 text-center hover:bg-white hover:bg-opacity-20 active:bg-opacity-60 sm:w-24 md:w-28"
                 >
-                  <button
-                    ref={aboutButtonRef}
-                    onClick={aboutToggle}
-                    className="w-20 rounded border border-white py-2 text-center hover:bg-white hover:bg-opacity-20 active:bg-opacity-60 sm:w-24 md:w-28"
-                  >
-                    About
-                  </button>
-                  <div className="mx-auto my-auto h-0 w-4 border-[0.5px] border-white md:w-8" />
+                  About
+                </button>
+                <div className="mx-auto my-auto h-0 w-4 border-[0.5px] border-white md:w-8" />
 
-                  <Link
-                    href="/projects"
-                    className="w-20 rounded border border-white py-2 text-center text-white hover:bg-white hover:bg-opacity-20 active:bg-opacity-60 sm:w-24 md:w-28"
-                  >
-                    Projects
-                  </Link>
+                <Link
+                  href="/projects"
+                  className="w-20 rounded border border-white py-2 text-center text-white hover:bg-white hover:bg-opacity-20 active:bg-opacity-60 sm:w-24 md:w-28"
+                >
+                  Projects
+                </Link>
 
-                  <div className="mx-auto my-auto h-0 w-4 border-[0.5px] border-white md:w-8" />
+                <div className="mx-auto my-auto h-0 w-4 border-[0.5px] border-white md:w-8" />
 
-                  <Link
-                    href="/blog"
-                    className="w-20 rounded border border-white py-2 text-center text-white hover:bg-white hover:bg-opacity-20 active:bg-opacity-60 sm:w-24 md:w-28"
-                  >
-                    Blog
-                  </Link>
+                <Link
+                  href="/blog"
+                  className="w-20 rounded border border-white py-2 text-center text-white hover:bg-white hover:bg-opacity-20 active:bg-opacity-60 sm:w-24 md:w-28"
+                >
+                  Blog
+                </Link>
 
-                  <div className="mx-auto my-auto h-0 w-4 border-[0.5px] border-white md:w-8" />
+                <div className="mx-auto my-auto h-0 w-4 border-[0.5px] border-white md:w-8" />
 
-                  <button
-                    ref={contactButtonRef}
-                    onClick={contactToggle}
-                    className="w-20 rounded border border-white py-2 text-center hover:bg-white hover:bg-opacity-20 active:bg-opacity-60 sm:w-24 md:w-28"
-                  >
-                    Contact
-                  </button>
-                </div>
-              ) : (
-                <div ref={linkRef} className="flex flex-col mt-5">
-                  <button
-                    ref={aboutButtonRef}
-                    onClick={aboutToggle}
-                    className="w-36 mb-4 rounded border border-white py-4 text-center hover:bg-white hover:bg-opacity-20 active:bg-opacity-60"
-                  >
-                    About
-                  </button>
-                  <Link
-                    href="/projects"
-                    className="w-36 mb-4 rounded border border-white py-4 text-center text-white hover:bg-white hover:bg-opacity-20 active:bg-opacity-60"
-                  >
-                    Projects
-                  </Link>
-                  <Link
-                    href="/blog"
-                    className="w-36 mb-4 rounded border border-white py-4 text-center text-white hover:bg-white hover:bg-opacity-20 active:bg-opacity-60"
-                  >
-                    Blog
-                  </Link>
-                  <button
-                    ref={contactButtonRef}
-                    onClick={contactToggle}
-                    className="w-36 mb-4 rounded border border-white py-4 text-center hover:bg-white hover:bg-opacity-20 active:bg-opacity-60"
-                  >
-                    Contact
-                  </button>
-                </div>
-              )}
+                <button
+                  ref={contactButtonRef}
+                  onClick={contactToggle}
+                  className="w-20 rounded border border-white py-2 text-center hover:bg-white hover:bg-opacity-20 active:bg-opacity-60 sm:w-24 md:w-28"
+                >
+                  Contact
+                </button>
+              </div>
+
+              <div ref={linkRef} className="flex flex-col mt-5 md:hidden">
+                <button
+                  ref={aboutButtonRef}
+                  onClick={aboutToggle}
+                  className="w-36 mb-4 rounded border border-white py-4 text-center hover:bg-white hover:bg-opacity-20 active:bg-opacity-60"
+                >
+                  About
+                </button>
+                <Link
+                  href="/projects"
+                  className="w-36 mb-4 rounded border border-white py-4 text-center text-white hover:bg-white hover:bg-opacity-20 active:bg-opacity-60"
+                >
+                  Projects
+                </Link>
+                <Link
+                  href="/blog"
+                  className="w-36 mb-4 rounded border border-white py-4 text-center text-white hover:bg-white hover:bg-opacity-20 active:bg-opacity-60"
+                >
+                  Blog
+                </Link>
+                <button
+                  ref={contactButtonRef}
+                  onClick={contactToggle}
+                  className="w-36 mb-4 rounded border border-white py-4 text-center hover:bg-white hover:bg-opacity-20 active:bg-opacity-60"
+                >
+                  Contact
+                </button>
+              </div>
             </nav>
           </div>
           <AboutMeModal
