dc9dd22e7bb6277fdc251201bb23ac509c5e333c
MikeFreno <michaelt.freno@gmail.com>
2023-07-21 22:40:33 -0400

    filled route

--- src/app/api/auth/callback/google/route.ts
+++ src/app/api/auth/callback/google/route.ts
@@ -36,48 +36,57 @@ export async function GET(request: NextRequest) {
 
     const userData = await userResponse.json();
     console.log(userData);
+    const name = userData.name;
+    const image = userData.picture;
+    const email = userData.email;
+    const email_verified = userData.email_verified;
 
-    //   const conn = ConnectionFactory();
+    const conn = ConnectionFactory();
 
-    //   const query = `SELECT * FROM User WHERE provider = ? AND display_name = ?`;
-    //   const params = ["google", ];
-    //   const res = await conn.execute(query, params);
-    //   if (res.rows[0]) {
-    //     cookies().set({
-    //       name: "userIDToken",
-    //       value: (res.rows[0] as User).id,
-    //       maxAge: 60 * 60 * 24 * 14,
-    //     });
-    //     if ((res.rows[0] as User).email) {
-    //       cookies().set({
-    //         name: "emailToken",
-    //         value: (res.rows[0] as User).email!,
-    //         maxAge: 60 * 60 * 24 * 14,
-    //       });
-    //     }
-    //   } else {
-    //     const icon = user.avatar_url;
-    //     const email = user.email;
-    //     const userId = uuidV4();
+    const query = `SELECT * FROM User WHERE provider = ? AND email = ?`;
+    const params = ["google", email];
+    const res = await conn.execute(query, params);
+    if (res.rows[0]) {
+      cookies().set({
+        name: "userIDToken",
+        value: (res.rows[0] as User).id,
+        maxAge: 60 * 60 * 24 * 14,
+      });
+      if ((res.rows[0] as User).email) {
+        cookies().set({
+          name: "emailToken",
+          value: (res.rows[0] as User).email!,
+          maxAge: 60 * 60 * 24 * 14,
+        });
+      }
+    } else {
+      const userId = uuidV4();
 
-    //     const insertQuery = `INSERT INTO User (id, email, display_name, provider, image) VALUES (?, ?, ?, ?, ?)`;
-    //     const insertParams = [userId, email, login, "github", icon];
-    //     const insertRes = await conn.execute(insertQuery, insertParams);
-    //     console.log(insertRes);
+      const insertQuery = `INSERT INTO User (id, email, email_verified, display_name, provider, image) VALUES (?, ?, ?, ?, ?)`;
+      const insertParams = [
+        userId,
+        email,
+        email_verified,
+        name,
+        "google",
+        image,
+      ];
+      const insertRes = await conn.execute(insertQuery, insertParams);
+      console.log(insertRes);
 
-    //     cookies().set({
-    //       name: "userIDToken",
-    //       value: userId,
-    //       maxAge: 60 * 60 * 24 * 14,
-    //     });
-    //     if (email) {
-    //       cookies().set({
-    //         name: "emailToken",
-    //         value: email,
-    //         maxAge: 60 * 60 * 24 * 14,
-    //       });
-    //     }
-    //   }
+      cookies().set({
+        name: "userIDToken",
+        value: userId,
+        maxAge: 60 * 60 * 24 * 14,
+      });
+      if (email) {
+        cookies().set({
+          name: "emailToken",
+          value: email,
+          maxAge: 60 * 60 * 24 * 14,
+        });
+      }
+    }
 
     return NextResponse.redirect(`${env.NEXT_PUBLIC_DOMAIN}/account`);
   } else {
