d253591fec9e3b44996f50d4e098ba3c09acd325
MikeFreno <michaelt.freno@gmail.com>
2023-07-21 22:26:22 -0400

    working through google flow

--- src/app/api/auth/callback/google/route.ts
+++ src/app/api/auth/callback/google/route.ts
@@ -22,62 +22,65 @@ export async function GET(request: NextRequest) {
         grant_type: "authorization_code",
       }),
     });
-    console.log("RES: " + tokenResponse);
+
     const { access_token } = await tokenResponse.json();
-    console.log("TOKEN: " + access_token);
 
-    const userResponse = await fetch("https://api.github.com/user", {
-      headers: {
-        Authorization: `token ${access_token}`,
-      },
-    });
-
-    const user = await userResponse.json();
-    const login = user.login;
-    const conn = ConnectionFactory();
-
-    const query = `SELECT * FROM User WHERE provider = ? AND display_name = ?`;
-    const params = ["github", login];
-    const res = await conn.execute(query, params);
-    if (res.rows[0]) {
-      cookies().set({
-        name: "userIDToken",
-        value: (res.rows[0] as User).id,
-        maxAge: 60 * 60 * 24 * 14,
-      });
-      if ((res.rows[0] as User).email) {
-        cookies().set({
-          name: "emailToken",
-          value: (res.rows[0] as User).email!,
-          maxAge: 60 * 60 * 24 * 14,
-        });
+    const userResponse = await fetch(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
       }
-    } else {
-      const icon = user.avatar_url;
-      const email = user.email;
-      const userId = uuidV4();
+    );
 
-      const insertQuery = `INSERT INTO User (id, email, display_name, provider, image) VALUES (?, ?, ?, ?, ?)`;
-      const insertParams = [userId, email, login, "github", icon];
-      const insertRes = await conn.execute(insertQuery, insertParams);
-      console.log(insertRes);
+    const userData = await userResponse.json();
+    console.log(userData);
 
-      cookies().set({
-        name: "userIDToken",
-        value: userId,
-        maxAge: 60 * 60 * 24 * 14,
-      });
-      if (email) {
-        cookies().set({
-          name: "emailToken",
-          value: email,
-          maxAge: 60 * 60 * 24 * 14,
-        });
-      }
-    }
+    //   const conn = ConnectionFactory();
 
-    return NextResponse.redirect(`${env.NEXT_PUBLIC_DOMAIN}/account`);
-  } else {
+    //   const query = `SELECT * FROM User WHERE provider = ? AND display_name = ?`;
+    //   const params = ["google", ];
+    //   const res = await conn.execute(query, params);
+    //   if (res.rows[0]) {
+    //     cookies().set({
+    //       name: "userIDToken",
+    //       value: (res.rows[0] as User).id,
+    //       maxAge: 60 * 60 * 24 * 14,
+    //     });
+    //     if ((res.rows[0] as User).email) {
+    //       cookies().set({
+    //         name: "emailToken",
+    //         value: (res.rows[0] as User).email!,
+    //         maxAge: 60 * 60 * 24 * 14,
+    //       });
+    //     }
+    //   } else {
+    //     const icon = user.avatar_url;
+    //     const email = user.email;
+    //     const userId = uuidV4();
+
+    //     const insertQuery = `INSERT INTO User (id, email, display_name, provider, image) VALUES (?, ?, ?, ?, ?)`;
+    //     const insertParams = [userId, email, login, "github", icon];
+    //     const insertRes = await conn.execute(insertQuery, insertParams);
+    //     console.log(insertRes);
+
+    //     cookies().set({
+    //       name: "userIDToken",
+    //       value: userId,
+    //       maxAge: 60 * 60 * 24 * 14,
+    //     });
+    //     if (email) {
+    //       cookies().set({
+    //         name: "emailToken",
+    //         value: email,
+    //         maxAge: 60 * 60 * 24 * 14,
+    //       });
+    //     }
+    //   }
+
+    //   return NextResponse.redirect(`${env.NEXT_PUBLIC_DOMAIN}/account`);
+    // } else {
     console.log("no code on callback");
   }
 }
