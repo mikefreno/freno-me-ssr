b97b8a97d7747e0e81d1a95e1252ca32ce9c8755
MikeFreno <michaelt.freno@gmail.com>
2023-07-22 21:45:14 -0400

    testing summin

--- src/app/blog/[title]/page.tsx
+++ src/app/blog/[title]/page.tsx
@@ -63,20 +63,20 @@ export default async function DynamicBlogPost({
   } else if (blog) {
     return (
       <div className="min-h-screen select-none">
-        <div className="relative z-30 overflow-hidden">
-          <div className="page-fade-in z-20 h-[50svh] mx-auto">
-            <div className="fixed w-full h-[50svh] brightness-75 image-overlay">
+        <div className="relative z-10 overflow-hidden">
+          <div className="h-72 sm:h-96 md:h-[50vh] mx-auto">
+            <div className="fixed w-full h-72 sm:h-96 md:h-[50vh] brightness-75 image-overlay">
               <Image
                 src={blog.banner_photo ? blog.banner_photo : "/bitcoin.jpg"}
                 alt="post-cover"
                 height={400}
                 width={600}
                 priority={true}
-                className="object-cover w-full h-full"
+                className="object-cover w-full h-full z--10"
               />
             </div>
             <div
-              className={`text-shadow absolute md:fixed top-[20svh] w-full brightness-150 z-10 select-text text-center tracking-widest text-white`}
+              className={`text-shadow absolute md:fixed top-28 sm:top-40 md:top-[20vh] w-full brightness-150 z-10 select-text text-center tracking-widest text-white`}
               style={{ pointerEvents: "none" }}
             >
               <div className="z-10 font-light tracking-widest text-3xl">
@@ -88,7 +88,7 @@ export default async function DynamicBlogPost({
             </div>
           </div>
         </div>
-        <div className="z-[500]">
+        <div className="z-40">
           <div className="flex justify-end pr-12 py-4 my-auto">
             <a href="#comments" className="mx-2">
               <div className="flex flex-col tooltip">
