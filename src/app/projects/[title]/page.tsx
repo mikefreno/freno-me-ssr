08867cf9c3e8a22e5214a9bb0d0962ea5dcf315f
MikeFreno <michaelt.freno@gmail.com>
2023-07-22 22:01:28 -0400

    oop

--- src/app/projects/[title]/page.tsx
+++ src/app/projects/[title]/page.tsx
@@ -78,7 +78,7 @@ export default async function DynamicProjectPost({
               />
             </div>
             <div
-              className={`text-shadow fixed h-32 sm:top-40 md:top-[20vh] w-full brightness-150 z-10 select-text text-center tracking-widest text-white`}
+              className={`text-shadow fixed top-32 sm:top-40 md:top-[20vh] w-full brightness-150 z-10 select-text text-center tracking-widest text-white`}
               style={{ pointerEvents: "none" }}
             >
               <div className="z-10 font-light tracking-widest text-3xl">
