65b43a5baf64dbac4293c9a2cb822084bc4f7386
MikeFreno <michaelt.freno@gmail.com>
2023-08-01 17:16:21 -0400

    fix sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: ["/", "/blog", "/projects"],
       disallow: ["/login", "/debug/**", "/databaseMGMT"],
     },
-    sitemap: "https://acme.com/sitemap.xml",
+    sitemap: "https://www.freno.me/sitemap.xml",
   };
 }
