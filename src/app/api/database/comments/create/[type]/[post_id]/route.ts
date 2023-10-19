c378dcd7ceda0fad0c103d0b9c980c242e708402
Michael Freno <michaelt.freno@gmail.com>
2023-10-19 19:43:59 -0400

    cleaned up api

--- src/app/api/database/comments/create/[type]/[post_id]/route.ts
+++ src/app/api/database/comments/create/[type]/[post_id]/route.ts
@@ -3,27 +3,31 @@ import { NextRequest, NextResponse } from "next/server";
 
 interface InputData {
   body: string;
-  parent_comment_id: number;
-  commenter_id: string;
+  parentCommentID: number;
+  commenterID: string;
 }
 
 export async function POST(
   input: NextRequest,
   context: { params: { type: string; post_id: string } },
 ) {
-  const data = (await input.json()) as InputData;
-  const { body, parent_comment_id, commenter_id } = data;
-  const conn = ConnectionFactory();
-  const query = `
+  try {
+    const data = (await input.json()) as InputData;
+    const { body, parentCommentID, commenterID } = data;
+    const conn = ConnectionFactory();
+    const query = `
     INSERT INTO Comment (body, ${context.params.type}_id, parent_comment_id, commenter_id)
     VALUES (?, ?, ?, ?)
     `;
-  const params = [
-    body,
-    parseInt(context.params.post_id),
-    parent_comment_id,
-    commenter_id,
-  ];
-  const res = await conn.execute(query, params);
-  return NextResponse.json({ data: res.insertId }, { status: 201 });
+    const params = [
+      body,
+      parseInt(context.params.post_id),
+      parentCommentID,
+      commenterID,
+    ];
+    const res = await conn.execute(query, params);
+    return NextResponse.json({ data: res.insertId }, { status: 201 });
+  } catch (e) {
+    return NextResponse.json({ error: e }, { status: 500 });
+  }
 }
--- src/app/api/user-data/get-all-users/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { ConnectionFactory } from "@/app/utils";
-
-export async function GET() {
-  const conn = ConnectionFactory();
-  const userQuery = "SELECT * FROM User";
-  const res = await conn.execute(userQuery);
-  return NextResponse.json({ users: res.rows }, { status: 200 });
-}
