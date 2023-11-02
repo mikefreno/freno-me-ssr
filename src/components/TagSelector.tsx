782b4b634bc6aa0f04fc9ce09cb7f7df7229f631
Michael Freno <michaelt.freno@gmail.com>
2023-11-02 16:54:09 -0400

    mobile ui fix

--- src/components/TagSelector.tsx
+++ src/components/TagSelector.tsx
@@ -60,7 +60,7 @@ export default function TagSelector(props: {
           props.category == "project"
             ? "border-blue-500 bg-blue-400 hover:bg-blue-500 dark:border-blue-700 dark:bg-blue-700 dark:hover:bg-blue-800"
             : "border-orange-500 bg-orange-400 hover:bg-orange-500 dark:border-orange-600 dark:bg-orange-600 dark:hover:bg-orange-700"
-        } rounded border px-4 py-2 text-white shadow-md transition-all duration-300  ease-in-out active:scale-90`}
+        } rounded border px-4 py-2 text-white shadow-md transition-all duration-300  ease-in-out active:scale-90 mt-2 md:mt-0`}
       >
         Filters
       </button>
