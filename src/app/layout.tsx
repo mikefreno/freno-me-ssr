d82df6ea635aa4f2cfcf2773f81ee479e153c088
Michael Freno <michaelt.freno@gmail.com>
2024-02-21 23:27:06 -0500

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/content.scss";
 import { Source_Code_Pro } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const SCP = Source_Code_Pro({ subsets: ["latin"] });
 
@@ -21,11 +22,12 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <body
-        className={`${SCP.className} bg-zinc-50 dark:bg-zinc-800 text-black dark:text-white`}
+        className={`${SCP.className} bg-zinc-50 text-black dark:bg-zinc-800 dark:text-white`}
       >
         <NextTopLoader showSpinner={false} color="#3b82f6" />
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
