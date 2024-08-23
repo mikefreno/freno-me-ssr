98ddc6673cd0bcce4f6cd1efc4cab5d5ee36d1d3
Michael Freno <michaelt.freno@gmail.com>
2024-08-22 21:26:53 -0400

    cors

--- next.config.js
+++ next.config.js
@@ -26,4 +26,25 @@ module.exports = {
 
     return config;
   },
+  async headers() {
+    return [
+      {
+        // matching all magic delve API routes
+        source: "/api/magic-delve:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
