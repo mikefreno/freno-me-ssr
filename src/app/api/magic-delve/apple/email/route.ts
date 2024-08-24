d009e8cd6f82af5483ab07f0500e29308889e19d
Michael Freno <michaelt.freno@gmail.com>
2024-08-24 02:24:07 -0400

    email retrieval

---
+++ src/app/api/magic-delve/apple/email/route.ts
@@ -0,0 +1,26 @@
+import { MagicDelveConnectionFactory } from "@/app/utils";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { userString } = await req.json();
+  if (!userString) {
+    return new NextResponse(
+      JSON.stringify({
+        success: false,
+        message: "Missing required fields",
+      }),
+      { status: 400, headers: { "content-type": "application/json" } },
+    );
+  }
+  const conn = MagicDelveConnectionFactory();
+  const query = "SELECT * FROM Users WHERE apple_user_string = ?";
+  const res = await conn.execute({ sql: query, args: [userString] });
+  if (res.rows.length > 0) {
+    return NextResponse.json(
+      { success: true, email: res.rows[0].email },
+      { status: 200 },
+    );
+  } else {
+    return NextResponse.json({ success: false }, { status: 404 });
+  }
+}
