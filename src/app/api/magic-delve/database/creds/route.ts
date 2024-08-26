69d72d89004423c4d2e2415c408f2e4e1c834380
Michael Freno <michaelt.freno@gmail.com>
2024-08-25 23:47:15 -0400

    implement per provider validity checking

--- src/app/api/magic-delve/database/creds/route.ts
+++ src/app/api/magic-delve/database/creds/route.ts
@@ -2,13 +2,17 @@ import { env } from "@/env.mjs";
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { MagicDelveConnectionFactory } from "@/app/utils";
+import { OAuth2Client } from "google-auth-library";
+const CLIENT_ID = env.NEXT_PUBLIC_GOOGLE_CLIENT_ID_MAGIC_DELVE;
+
+const client = new OAuth2Client(CLIENT_ID);
 
 export async function POST(req: NextRequest) {
   const authHeader = req.headers.get("authorization");
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return new NextResponse(JSON.stringify({ valid: false }), { status: 401 });
   }
-  const { email } = await req.json();
+  const { email, provider } = await req.json();
   if (!email) {
     return new NextResponse(
       JSON.stringify({ success: false, message: "missing email in body" }),
@@ -21,12 +25,33 @@ export async function POST(req: NextRequest) {
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, env.JWT_SECRET_KEY) as jwt.JwtPayload;
+    let valid_request = false;
+    if (provider == "email") {
+      const decoded = jwt.verify(token, env.JWT_SECRET_KEY) as jwt.JwtPayload;
+      if (decoded.email == email) {
+        valid_request = true;
+      }
+    } else if (provider == "google") {
+      const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,
+      });
+      if (ticket.getPayload()?.email == email) {
+        valid_request = true;
+      }
+    } else {
+      const conn = MagicDelveConnectionFactory();
+      const query = "SELECT * FROM User WHERE apple_user_string = ?";
+      const res = await conn.execute({ sql: query, args: [token] });
+      if (res.rows.length > 0 && res.rows[0] && res.rows[0].email == email) {
+        valid_request = true;
+      }
+    }
 
-    if (decoded.email == email) {
+    if (valid_request) {
       const conn = MagicDelveConnectionFactory();
       const query = "SELECT * FROM User WHERE email = ? LIMIT 1";
-      const params = [decoded.email];
+      const params = [email];
       const res = await conn.execute({ sql: query, args: params });
       if (res.rows.length === 1) {
         const user = res.rows[0];
