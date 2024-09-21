5e79f4a91821637de9bd3aba07d4012f9c3c43c6
Michael Freno <michaelt.freno@gmail.com>
2024-09-21 11:59:50 -0400

    magic delve token route

---
+++ src/app/api/magic-delve/tokens/route.ts
@@ -0,0 +1,29 @@
+import { MagicDelveConnectionFactory } from "@/app/utils";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { token } = await req.json();
+  if (!token) {
+    return new NextResponse(
+      JSON.stringify({ success: false, message: "missing token in body" }),
+      {
+        status: 401,
+      },
+    );
+  }
+  const conn = MagicDelveConnectionFactory();
+  const query = "SELECT * FROM Token WHERE token = ?";
+  const res = await conn.execute({ sql: query, args: [token] });
+  if (res.rows.length > 0) {
+    // Update the last_updated_at field if the token exists
+    const queryUpdate =
+      "UPDATE Token SET last_updated_at = datetime('now') WHERE token = ?";
+    const resUpdate = await conn.execute({ sql: queryUpdate, args: [token] });
+    return NextResponse.json(JSON.stringify(resUpdate));
+  } else {
+    // Insert the token if it doesn't exist
+    const queryInsert = "INSERT INTO Token (token) VALUES (?)";
+    const resInsert = await conn.execute({ sql: queryInsert, args: [token] });
+    return NextResponse.json(JSON.stringify(resInsert));
+  }
+}
